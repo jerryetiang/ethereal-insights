a32c8ac683fb6516f57663d37d9186f77e925c2b
jerryetiang <jerryetiang@icloud.com>
2023-12-01 18:01:18 +0000

    abakttics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import "animate.css";
 import Provider from "@/utils/provider";
 import NavigationBar from "@/ui/components/navbar/navbar";
+import { Analytics } from '@vercel/analytics/react';
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({
         <Provider>
           <NavigationBar />
           <main className="">{children}</main>
+          <Analytics />
         </Provider>
       </body>
     </html>
