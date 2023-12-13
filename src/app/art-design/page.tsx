740b6fb8063f3a7b2e233440debea904024a2e85
jerryetiang <jerryetiang@icloud.com>
2023-12-13 19:21:20 +0300

    .

--- src/app/art-design/page.tsx
+++ src/app/art-design/page.tsx
@@ -22,7 +22,7 @@ export default async function Art() {
         subtitle="Discover captivating visual narratives, explore artistic expressions, and witness the transformative power of design."
       />
       <ArticleGrid>
-        {data.map((article) => (
+        {data.map((article: { id: React.Key | null | undefined; title: string; author: string; date: string; content: string; }) => (
           <Article
             key={article.id}
             title={article.title}
